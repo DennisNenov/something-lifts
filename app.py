f30f391a7b48e12a0e398130c09c57b03a4b2c92
unknown <eshoyfer@gmail.com>
2015-05-18 21:19:28 -0400

    Login stuff, flashing, Mongo, session check, added blank templates for dashboard, renamed signup to register

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask,render_template, session, redirect, request, url_for, g
+from flask import Flask,render_template, session, redirect, request, url_for, g, flash
 from functools import wraps
 from pymongo import Connection
+import mongo
 import tools
 import auth
 
@@ -8,10 +9,13 @@ conn = Connection()
 db = conn['a']
 app = Flask(__name__)
 
+NOT_LOGGED_IN = "You are not logged in!"
+ALREADY_LOGGED_IN = "You are already logged in!"
+
 def login_required(f):
 	@wraps(f)
 	def decorated_function(*args, **kwargs):
-		not_logged_in = g.user is None
+		not_logged_in = 'user' not in session
 
 		if not_logged_in:
 			return redirect(url_for('login', next=request.url))
@@ -21,6 +25,15 @@ def login_required(f):
 @app.route("/") 
 def home():
     return render_template("index.html")
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    if 'user' in session:
+        return render_template('dashboard.html')
+    else:
+        flash(NOT_LOGGED_IN)
+        return redirect(url_for('login')) 
 
 @app.route("/macros")
 @login_required
@@ -52,28 +65,67 @@ def goals():
        #keep copy+pasting and changing the goals to the different goals youre going to include
     return render_template("goals.html")
 
-@app.route("/login")
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    if request.method == "POST":
-        username = request.form.get("username", None) #name the element in the html form username; return none if no value is found in username
-        password = request.form.get("password", None)
-        if db.users.find_one({'name': username, 'password': password}) != None:
-        	g.user = username
-            return redirect(url_for('goals'))
-    return render_template("login.html", username=username)
+    if 'user' in session:
+        flash(ALREADY_LOGGED_IN)
+        return redirect(url_for('dashboard'))
 
-@app.route("/signup")
-def signup():
-    if request.method == "POST":
-        username = request.form.get("username", None)
-        password = request.form.get("password", None)
-        db.users.insert({'name': username, 'password': password, 'weight': 0, 'goal': 'None'})
-        return redirect(url_for('login'))
+    # Logging in
+    if request.method == 'POST':
+        user = request.form['user']
+        pwd = request.form['pwd']
 
-@app.route("/logout")
+        # Login attempt
+        result, passed = mongo.authenticate_user(user, pwd)
+
+        if result: # Success
+            session['user'] = user
+            flash(passed)
+            #redirect(url_for('dashboard'))
+            return redirect('dashboard')
+
+        else: # Failure
+            flash(passed)
+            return render_template("login.html")
+    else:
+        return render_template("login.html")
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if 'user' in session:
+        flash(ALREADY_LOGGED_IN)
+        return redirect(url_for('dashboard'))
+
+    # Registration form submitted
+    if request.method == 'POST':
+        user = request.form['user']
+        pwd = request.form['pwd']
+
+        # Reigstration attempt
+        result, passed = mongo.create_user(user, pwd)
+
+        if result: #Success
+            flash(passed)
+            return redirect(url_for('login'))
+
+        else: # Failure
+            flash(passed)
+            return redirect(url_for('register'))
+
+    # Viewing registration page
+    else:
+        return render_template('register.html')
+
+#The original code for register had the following:
+#db.users.insert({'name': username, 'password': password, 'weight': 0, 'goal': 'None'})
+#Perhaps we should come with our standardized form of storing a user? (what we'll need etc) 
+#Until then I'll use a generic one. 
+
+@app.route('/logout')
 def logout():
-    session.pop('username',None)
-    return redirect("/")
+    session.pop('user', None)
+    return redirect(url_for('home'))
 
 if __name__=="__main__":
     app.debug=True
